994d33d76b98d720cf08bcfd53ca5780dd8e29bf
Char_XL <1759707312@qq.com>
2024-10-16 20:42:32 +0800

    提交

--- user/sbrk1.c
+++ user/sbrk1.c
@@ -5,8 +5,12 @@
 int
 main()
 {
+    int a = 0;
+    a = 1;
+    // $1 = 0x8000000000087f49
     char *new_memory = (char *)sbrk(1);
-
+    // $4 = 0x8000000000087f64
+    a = 2;
     if(new_memory == (char *) -1){
         printf("sbrk failed to allocate memory.\n");
         exit(-1);
